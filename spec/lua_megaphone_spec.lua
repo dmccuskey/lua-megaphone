8d65903ddca8689f73dc80dc73862c0e0cb5ce7b
dmc <my-github@davidmccuskey.com>
2015-02-11 01:15:28 -0700

    update megaphone to send data with params. add tests

--- spec/lua_megaphone_spec.lua
+++ spec/lua_megaphone_spec.lua
@@ -94,17 +94,49 @@ describe( "Module Test: lua_megaphone.lua", function()
 		end)
 
 		it( "can setup listener and get events", function()
-			local event
+			local event, data
 			local etype = 'bogus_event'
 			local callback = function( e )
 				event = e
 			end
+
+			-- test no data
 
 			assert.is_not.error( function() Megaphone:listen( callback ) end )
 			assert.is_not.error( function() Megaphone:say( etype ) end )
 
 			assert.is.equal( event.name, 'megaphone_event' )
 			assert.is.equal( event.type, etype )
+			assert.is.equal( event.data, nil )
+
+			event = nil
+
+			-- test string data
+
+			assert.is_not.error( function() Megaphone:say( etype, "hello" ) end )
+
+			assert.is.equal( event.name, 'megaphone_event' )
+			assert.is.equal( event.type, etype )
+			assert.is.equal( event.data, "hello" )
+
+			event = nil
+
+			data = {one=1}
+			assert.is_not.error( function() Megaphone:say( etype, data ) end )
+
+			assert.is.equal( event.name, 'megaphone_event' )
+			assert.is.equal( event.type, etype )
+			assert.is.equal( event.data, data )
+
+			event = nil
+
+			data = {one=1,two=2}
+			assert.is_not.error( function() Megaphone:say( etype, data, {merge=true} ) end )
+
+			assert.is.equal( event.name, 'megaphone_event' )
+			assert.is.equal( event.type, etype )
+			assert.is.equal( event.one, data.one )
+			assert.is.equal( event.two, data.two )
 
 			event = nil
 
@@ -113,6 +145,15 @@ describe( "Module Test: lua_megaphone.lua", function()
 
 			assert.is.equal( event, nil )
 
+		end)
+
+		it( "throws errors with improper params", function()
+			local etype = 'bogus_event'
+			local data = "hello"
+			assert.is.error( function() Megaphone:say( etype, data, "params" ) end )
+			assert.is.error( function() Megaphone:say( etype, data, 2 ) end )
+			assert.is.error( function() Megaphone:say( etype, nil, "params" ) end )
+			assert.is.error( function() Megaphone:say( etype, nil, 2 ) end )
 		end)
 
 	end)
